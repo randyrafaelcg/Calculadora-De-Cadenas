e093b03b109c752d4a1971e5bf47be0ef59d77b7
randyrafaelcg <randyrafaelcg@gmail.com>
2021-10-04 14:17:49 -0400

    Se puede reconocer mas caracteres para separar los  numeros

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function Calcular(numeros) {
   let deli='//\n'
-  let delimitador=/[,-]+/;
+  let delimitador=/\W|[,-]+/;
   let list=numeros.split(delimitador);
   let resp=0;
   for (let i = 0; i < list.length; i++) {
